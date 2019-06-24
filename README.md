6c0ec08f78a14c61fba736483c37ff2e4be1e20b
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-06-24 16:27:04 +0200

    tests, readme, and script

---
+++ README.md
@@ -0,0 +1,27 @@
+# DocumentationGeneratorRegistry
+
+This registry allows package authors to specify where to find and how to build the documentation for their
+packages displayed at [pkg.julialang.org](https://pkg.julialang.org/docs/).
+
+## Usage
+Just add a new entry to `Registry.toml` by hand (see `Specifications` below) or with the provided `add_to_registry.jl` script:
+```
+julia add_to_registry.jl Package1 Package2 Package3
+```
+
+## Options
+
+There are three options for specifying how (with the `method` key) and where (with the `location` key) to find a package's documentation:
+- `vendored`: The documentation *source* is located in a folder (specified by `location`) in the package's root directory.
+- `git-repo`: The documentation *source* is located in the git repo given by `location`.
+- `hosted`: The *built* documentation is reachable under the URL given by `location`.
+
+## Specifications
+
+Each package is specified by UUID, which is also the top-level key. Required sub-keys are `method`, `location`, and `name`:
+```
+[uuid]
+name = "PackageName"
+method = "hosted"
+location = "https://thisismy.hosted.documentation.org/stable"
+```
---
+++ test/runtests.jl
@@ -0,0 +1,76 @@
+using Test, Pkg, UUIDs, HTTP
+
+const REGISTRY_PATH = joinpath(@__DIR__, "..", "Registry.toml")
+const VALID_METHODS = ("hosted", "git-repo", "vendored")
+
+isurlish(url) = occursin(r"(http|https|git)\://.*", url)
+
+function testkeyvalidity(registry, key)
+    uuid = UUID(key)
+
+    entry = registry[key]
+
+    if !haskey(entry, "name")
+        @error("`name` entry missing for `$(uuid)`.")
+        return false
+    end
+    name = entry["name"]
+
+    method = ""
+    if haskey(entry, "method")
+        method = entry["method"]
+        if !(method in VALID_METHODS)
+            @error("Invalid method `$(method)` specified for `$(name)` ($(uuid)).")
+            return false
+        end
+    else
+        @error("No `method` specified for `$(name)` ($(uuid)).")
+        return false
+    end
+
+    if haskey(entry, "location")
+        uri = entry["location"]
+        if method in ("git-repo", "hosted")
+            if isurlish(uri)
+                req = try
+                    HTTP.request("GET", uri)
+                    if req.status > 400
+                        @warn("""
+                        `$(uri)` returned `$(req.status)` for `$(name)` ($(uuid)).
+                        Please double check the URL.
+                        """)
+                    end
+                catch err
+                    @warn("""
+                        `$(uri)` request failed for `$(name)` ($(uuid)).
+                        Please double check the URL.
+                    """)
+                end
+            else
+                @error("Invalid URL `$(uri)` for `$(name)` ($(uuid)).")
+                return false
+            end
+        elseif method == "vendored"
+            if isurlish(uri)
+                @error("`location` should not be a URL for vendored docs in `$(name)` ($(uuid)).")
+                return false
+            end
+        end
+    else
+        @error("No `location` specified for `$(name)` ($(uuid)).")
+        return false
+    end
+
+    return true
+end
+
+
+@testset "Registry Validity" begin
+    @test isfile(REGISTRY_PATH)
+    toml = Pkg.TOML.parsefile(REGISTRY_PATH)
+    @test collect(unique(keys(toml))) == collect(keys(toml))
+    @testset "Package: $(key)" for key in keys(toml)
+        isvalid = testkeyvalidity(toml, key)
+        @test isvalid
+    end
+end
